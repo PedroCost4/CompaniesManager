da00936af09ed49f749003bcd3930d1fb0a1dc25
Pedro Costa <pedro.costacdc@gmail.com>
2023-08-07 17:24:38 -0300

    refactor: async and prettier format

--- apps/server/src/modules/office/useCases/delete.ts
+++ apps/server/src/modules/office/useCases/delete.ts
@@ -1,10 +1,10 @@
 import prisma from "../../../db";
 
 export const deleteOffice = async (id: string) => {
-    const office = await prisma.office.delete({
-        where: {
-        id,
-        },
-    });
-    return office;
-}
+  const office = prisma.office.delete({
+    where: {
+      id,
+    },
+  });
+  return office;
+};
