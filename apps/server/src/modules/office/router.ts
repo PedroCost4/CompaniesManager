10af2eb73b7a3823eaafedf993968379f77948ee
Pedro Costa <pedro.costacdc@gmail.com>
2023-08-07 17:25:05 -0300

    feat: office router changes

--- apps/server/src/modules/office/router.ts
+++ apps/server/src/modules/office/router.ts
@@ -2,10 +2,18 @@
 import { router } from "../../trpc";
 import { inferRouterInputs, inferRouterOutputs } from "@trpc/server";
 import { createOfficeProcedure } from "./controllers/create";
+import { updateOfficeProcedure } from "./controllers/update";
+import { deleteOfficeProcedure } from "./controllers/delete";
+import { removeOfficeProcedure } from "./controllers/removeFromCompany";
+import { getOfficeProcedure } from "./controllers/get";
 
 export type OfficeRouterInputs = inferRouterInputs<typeof officeRouter>;
 export type OfficeRouterOutputs = inferRouterOutputs<typeof officeRouter>;
 
 export const officeRouter = router({
     createOffice: createOfficeProcedure,
+    updateOffice: updateOfficeProcedure,
+    deleteOffice: deleteOfficeProcedure,
+    removeOfficeFromCompany: removeOfficeProcedure,
+    getOffice: getOfficeProcedure,
 });
